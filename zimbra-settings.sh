d6b3f9be20d668652ad036b601826fd6adbc71fa
rpelletierrefocus <47399964+rpelletierrefocus@users.noreply.github.com>
2020-05-26 16:41:24 -0400

    Update zimbra-settings.sh

--- zimbra-settings.sh
+++ zimbra-settings.sh
@@ -9,3 +9,51 @@ do
 done
 
 zmprov sp admin@flmaine.com ${ADMINPASS}
+
+
+wget https://github.com/rpelletierrefocus/zimbra-rocket/blob/master/extension/rocket.jar
+cp extension.jar /opt/zimbra/lib/ext/rocket/
+ su - zimbra -c '/opt/zimbra/bin/zmmailboxdctl restart'
+
+
+/opt/zimbra/libexec/zmfixperms
+
+
+
+apt-get -y install zabbix-agent
+
+
+
+
+
+
+
+
+
+## Install zmbackup
+cd /tmp
+apt-get install parallel wget httpie sqlite3 -y
+git clone -b 1.2-version https://github.com/lucascbeyeler/zmbackup.git
+cd zmbackup
+./install.sh
+
+## Install filebeat
+
+
+# Install telegraf
+
+
+
+
+
+
+
+
+#### fstab
+10.100.61.20:/volume1/backup1/docker-containers /mnt/containers  nfs      defaults    0       0
+10.100.61.21:/volume1/mailstore  /backup/mailbox_backup  nfs      defaults    0       0
+/dev/sdb1    /opt/zimbra/opt/zimbra/store  ext4  defaults,auto,_netdev    0    0
+/dev/sdc1    /mailstore  ext4  defaults,auto,_netdev    0    0
+
+
+
