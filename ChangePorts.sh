4f309c7032fbcca850c5560af72d1504695d34a8
rpelletierrefocus <47399964+rpelletierrefocus@users.noreply.github.com>
2020-05-26 15:25:13 -0400

    Update ChangePorts.sh

--- ChangePorts.sh
+++ ChangePorts.sh
@@ -1,5 +1,7 @@
 zmprov ms $1 zimbraMailPort 60081
+zmprov ms $1 zimbraMailProxyPort 61081
 zmprov ms $1 zimbraMailSSLPort 60443
+zmprov ms $1 zimbraMailSSLProxyPort 61443
 zmprov ms $1 zimbraMtaAuthHost $1
 zmcontrol stop
 zmcontrol start
